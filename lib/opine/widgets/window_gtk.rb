5b747ec88514456b3bfda309065e70f2b437d6e4
Patrick Hanevold <patrick.hanevold@gmail.com>
2014-04-17 00:07:46 +0200

    gtk window

---
+++ lib/opine/widgets/window_gtk.rb
@@ -0,0 +1,22 @@
+class Opine::Native::Window < Opine::Window
+  def initialize(options,&block)
+    @window = Gtk::Window.new
+
+    super
+
+    window.set_default_size frame.width, frame.height
+    window.set_window_position :center
+
+    window.signal_connect "destroy" do 
+      Gtk.main_quit 
+    end
+
+    instance_eval(&block) if block
+
+    window.show
+  end
+
+  def title= name
+    window.set_title name
+  end
+end
